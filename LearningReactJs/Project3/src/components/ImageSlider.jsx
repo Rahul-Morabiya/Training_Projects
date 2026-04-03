f830a3bd1f39b8757e15386a4cdd3707e7cfeead
Rahul-Morabiya <rahulmorabiya1234@gmail.com>
2026-04-04 00:52:25 +0530

    Added Learning React Projects 1,2,3

---
+++ LearningReactJs/Project3/src/components/ImageSlider.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const ImageSlider = ({ url, limit, page = 1 }) => {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+    function handleNext(){
+        setCurrentSlide(currentSlide+1);
+    }
+    function handlePrevious(){
+        setCurrentSlide(currentSlide-1);
+    }
+
+  async function fetchImages(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        setLoading(false);
+        console.log(data);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+    console.log(images);
+  }, [url,page,limit]);
+
+  if (loading) {
+    return <div>Loading Data</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error Occured! {errorMsg}</div>;
+  }
+
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        gap: "30px",
+        flexWrap: "wrap",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      (
+        <div
+          className="item"
+          key={idx}
+          style={{
+            display: "flex",
+            height: "50px",
+            width: "50px",
+          }}
+        >
+          <img src={images[currentSlide].download_url} alt="" />
+        </div>
+      )
+      <button className="" style={{color:'white',background:'orange',height:'20px',width:'40px'}} onClick={handlePrevious()}>Previous</button>
+      <button className="" style={{color:'white',background:'orange',height:'20px',width:'40px'}} onClick={handleNext()}>Next</button>
+      
+    </div>
+  );
+};
+
+export default ImageSlider;
---
+++ LearningReactJs/Project3/src/components/StarRating.jsx
@@ -0,0 +1,35 @@
+import {FaStar} from 'react-icons/fa';
+import { useState } from 'react';
+import "./styles.css";
+export default function StarRating({noOfStars=5}){
+  
+    const [rating,setRating]=useState(0);
+    const [hover,setHover]=useState(0);
+    
+    function handleClick(getCurrentInd){
+        setRating(getCurrentInd);
+        console.log(getCurrentInd);
+    }
+
+    function handleMouseEnter(getCurrentInd){
+        setHover(getCurrentInd);
+        console.log("Mouse Enter");
+    }
+
+    function handleMouseLeave(){
+        setHover(rating);
+        console.log("Mouse Leave");
+    }
+  
+    return (
+    <div className='star-rating'>
+       {
+        [...Array(noOfStars)].map((_,index)=>{
+            index+=1;
+            return <FaStar key={index} className={index<=(hover||rating) ? 'active':'inactive'} onClick={()=>handleClick(index)} onMouseEnter={()=>handleMouseEnter(index)} onMouseLeave={()=>handleMouseLeave()} size={40}/>
+        })
+       }
+       </div>
+  )
+}
+
